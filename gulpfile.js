bd889aa9d0de8bb7dfa26d9b113875b984bbdfa1
Chris Blanks <chris.blanks@gmail.com>
2017-07-09 21:26:26 +0100

    gulp with sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp'),
   gutil = require('gulp-util'),
-  sass = require('gulp-ruby-sass');
+  sass = require('gulp-ruby-sass'),
+  jshint = require('gulp-jshint');
 
 // Create an electron-connect server to enable reloading
 const electron = require('electron-connect').server.create();
@@ -24,14 +25,22 @@ gulp.task('start', () => {
 
 });
 
-gulp.task('sass', () => {
+gulp.task('sass', function() {
+  gutil.log('Some SASS changed!!!');
+  return gulp.src('client/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('client'));
+});
 
-  gulp.watch('client/**/*.scss', () => {
-    gutil.log('Something changed!!!');
+gulp.task('jshint', function() {
+  gutil.log('Some JS changed!!!');
+  return gulp.src('client/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
 
-  	sass('client/index.scss')
-  		.on('error', sass.logError)
-  		.pipe(gulp.dest('client'));
-
-  });
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+  gulp.watch('client/**/*.js', ['jshint']);
+  gulp.watch('client/**/*.scss', ['sass']);
 });
